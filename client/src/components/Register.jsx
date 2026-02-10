5477f4a280a7a0b08d470515d75c5f1c906e7486
Sabin kc <1skc454@gmail.com>
2026-02-10 18:01:12 +0545

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -24,7 +24,7 @@ const Register = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:4000/api/auth/signup",
+        "https://noteapp-backend-nvje.onrender.com/api/auth/signup",
         user,
         { withCredentials: true },
       );
